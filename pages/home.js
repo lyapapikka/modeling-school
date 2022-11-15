aa3c29cf0aa25afd18cb7ec0ea4a703160701203
valeriyshashkin <velianjanjan@gmail.com>
2022-11-15 13:12:43 +0500

    снова дизайню

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Content from "../components/Content";
-import { PlusIcon } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useUser } from "@supabase/auth-helpers-react";
 
 export default function Home() {
   const router = useRouter();
   const user = true;
+  const [tab, setTab] = useState("posts");
+
+  const postsTab = () => setTab("posts");
+  const widgetsTab = () => setTab("widgets");
 
   useEffect(() => {
     if (!user) {
@@ -27,32 +30,38 @@ export default function Home() {
       </Head>
       <Content>
         <Header />
-        <div className="text-lg">Моя страница</div>
-        <div className="flex flex-col sm:flex-row sm:gap-2">
-          <div className="w-full">
-            <div className="bg-neutral-800 rounded-2xl mt-2 px-4 py-3 w-full">
-              <div className="flex justify-between">
-                Виджеты
-                <button className="sm:hover:bg-neutral-700 rounded-full p-2 -m-2">
-                  <PlusIcon className="w-6" />
-                </button>
+        <div className="flex space-x-2 mb-4 mt-2">
+          <button
+            onClick={postsTab}
+            className={`flex sm:hover:bg-neutral-700 px-3 py-1 rounded-full ${
+              tab === "posts" ? "bg-neutral-600" : "bg-neutral-800"
+            }`}
+          >
+            Записи
+          </button>
+          <button
+            onClick={widgetsTab}
+            className={`flex sm:hover:bg-neutral-700 px-3 py-1 rounded-full ${
+              tab === "widgets" ? "bg-neutral-600" : "bg-neutral-800"
+            }`}
+          >
+            Виджеты
+          </button>
+        </div>
+        <div className="bg-neutral-800 rounded-2xl mt-2 px-4 py-3 w-full">
+          {tab === "widgets" && (
+            <div className="w-full">
+              <div className="text-neutral-500 bg-neutral-800 rounded-2xl">
+                У вас пока нет виджетов
               </div>
             </div>
-            <div className="text-neutral-500 bg-neutral-800 rounded-2xl px-4 py-3 mt-2">
-              У вас пока нет виджетов
-            </div>
-          </div>
-          <div className="w-full">
-            <div className="bg-neutral-800 rounded-2xl mt-2 px-4 py-3">
-              <div className="flex justify-between">
-                Записи
-                <button className="sm:hover:bg-neutral-700 rounded-full p-2 -m-2">
-                  <PlusIcon className="w-6" />
-                </button>
+          )}
+          {tab === "posts" && (
+            <div className="w-full">
+              <div className="text-neutral-500 bg-neutral-800 rounded-2xl">
+                У вас пока нет записей
               </div>
-            </div>
-            <div className="text-neutral-500 bg-neutral-800 rounded-2xl px-4 py-3 mt-2">У вас пока нет записей</div>
-            {/* <div className="divide-y divide-neutral-500 bg-neutral-800 rounded-2xl px-4 py-3">
+              {/* <div className="divide-y divide-neutral-500 bg-neutral-800 rounded-2xl px-4 py-3">
               <div className="py-2">
                 Длинный текст записи, приведенный здесь лишь для демонстрации
                 того, как выглядят много строк текста в реальном интерфейсе
@@ -62,7 +71,8 @@ export default function Home() {
                 того, как выглядят много строк текста в реальном интерфейсе
               </div>
             </div> */}
-          </div>
+            </div>
+          )}
         </div>
       </Content>
     </>
