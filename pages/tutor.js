7d3ce676059ff8cce24f6ee60565f3f1d88e2701
valeriyshashkin <71643953+valeriyshashkin@users.noreply.github.com>
2022-11-30 10:02:01 +0000

    обновил ссылку на пдф и добавил поле поиска

--- pages/tutor.js
+++ pages/tutor.js
@@ -3,19 +3,31 @@ import Content from "../components/Content";
 import Header from "../components/Header";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import tutors from "../tutors";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowTopRightOnSquareIcon,
+  DocumentIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Tutor() {
   const { isLoading, session } = useSessionContext();
   const router = useRouter();
+  const [_origin, setOrigin] = useState("");
+  const [query, setQuery] = useState("");
+
+  const changeQuery = ({ target: { value } }) => setQuery(value);
 
   useEffect(() => {
     if (!isLoading && !session) {
       router.replace("/");
     }
   }, [isLoading, session, router]);
+
+  useEffect(() => {
+    setOrigin(origin);
+  }, []);
 
   if (isLoading || !session) {
     return null;
@@ -29,14 +41,22 @@ export default function Tutor() {
       <Content>
         <Header home bookPage />
         <div className="text-xl ml-4 mb-4 font-bold">Учебник</div>
+        <div className="relative px-2 mb-4">
+          <input
+            value={query}
+            onChange={changeQuery}
+            placeholder="Поиск..."
+            className="rounded-2xl bg-neutral-700 py-2 pr-3 w-full pl-11"
+          />
+          <MagnifyingGlassIcon className="w-6 stroke-neutral-500 absolute left-5 top-1/2 -translate-y-1/2" />
+        </div>
         {tutors.map((t, i) => (
           <a
-            target="_blanc"
-            className="sm:hover:bg-neutral-700 flex ml-auto block bg-neutral-800 text-sm font-medium px-4 py-2 rounded-2xl w-full"
-            href={`/tutors/${t.filename}`}
+            className="sm:hover:bg-neutral-700 flex ml-auto block bg-neutral-800 px-4 py-2 rounded-2xl w-full"
+            href={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${_origin}/tutors/${t.filename}`}
             key={i}
           >
-            <ArrowTopRightOnSquareIcon className="w-6 mr-2" />
+            <DocumentIcon className="w-6 mr-4 shrink-0" />
             {t.title}
           </a>
         ))}
