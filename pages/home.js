c1f631628cf3eb5c9b40a6edfcb3aeeacd532e23
valeriyshashkin <velianjanjan@gmail.com>
2023-01-05 21:32:33 +0500

    адаптировал пост под главную страницу

--- pages/home.js
+++ pages/home.js
@@ -4,53 +4,17 @@ import Content from "../components/Content";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSessionContext } from "@supabase/auth-helpers-react";
-import Link from "next/link";
-import Image from "next/image";
 import useSWRInfinite from "swr/infinite";
 import fetcher from "../utils/fetcher";
 import api from "../utils/api";
-import { formatRelative } from "date-fns";
-import russianLocale from "date-fns/locale/ru";
-import { toast } from "react-toastify";
-import ReactLinkify from "react-linkify";
-import { ArchiveBoxArrowDownIcon, LinkIcon } from "@heroicons/react/24/outline";
 import InfiniteScroll from "react-infinite-scroll-component";
+import Post from "../components/Post";
+import useSWR from "swr";
 
 export default function Home() {
   const router = useRouter();
   const [_origin, setOrigin] = useState("");
   const { isLoading, session, supabaseClient } = useSessionContext();
-
-  const addToArchive = async (post_id) => {
-    const { data } = await supabaseClient
-      .from("archive")
-      .select()
-      .eq("user_id", session.user.id)
-      .eq("post_id", post_id);
-
-    if (data.length === 0) {
-      await supabaseClient
-        .from("archive")
-        .insert([{ user_id: session.user.id, post_id }]);
-    }
-
-    toast.success("Запись сохранена в архиве", {
-      position: "bottom-right",
-      autoClose: 2000,
-      hideProgressBar: true,
-      closeOnClick: false,
-      pauseOnHover: false,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-      closeButton: false,
-      className: "bottom-14 sm:bottom-auto m-2",
-    });
-  };
-
-  const sharePost = (id) => {
-    navigator.share({ url: `${origin}/post/${id}` });
-  };
 
   const {
     data: posts,
@@ -68,6 +32,18 @@ export default function Home() {
             session
           );
         }
+      : null,
+    fetcher
+  );
+
+  const { data: archive, mutate: mutateArchive } = useSWR(
+    !isLoading && session && posts
+      ? api(
+          `archive?post_id=in.(${posts
+            .map((p) => p.map(({ id }) => id))
+            .join()}),posts(text,created_at,id,groups(name,id))`,
+          session
+        )
       : null,
     fetcher
   );
@@ -98,7 +74,7 @@ export default function Home() {
         <div className="text-xl font-bold pl-4 pb-4 bg-neutral-900 rounded-b-2xl">
           Главная
         </div>
-        {posts ? (
+        {posts && archive ? (
           posts[0].length === 0 ? (
             <div className="text-center text-neutral-500 mt-10">
               Новостей нет
@@ -118,88 +94,25 @@ export default function Home() {
             >
               {posts.map((post) =>
                 post.map((p) => (
-                  <div
-                    className="bg-neutral-900 rounded-2xl py-1 px-4"
-                    key={p.id}
-                  >
-                    <div>
-                      <div className="flex gap-4">
-                        <Link href={`/group/${p.groups.id}?from=home`}>
-                          <a className="mt-4 ml-2">
-                            <Image
-                              alt=""
-                              width={30}
-                              height={30}
-                              src={`https://avatars.dicebear.com/api/identicon/${p.groups.id}.svg`}
-                            />
-                          </a>
-                        </Link>
-                        <div>
-                          <Link href={`/group/${p.groups.id}?from=home`}>
-                            <a className="mt-2 inline-block">{p.groups.name}</a>
-                          </Link>
-                          <div className="text-neutral-500">
-                            {formatRelative(
-                              new Date(p.created_at),
-                              new Date(),
-                              {
-                                locale: russianLocale,
-                              }
-                            )}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="py-2 rounded-2xl pr-6 whitespace-pre-wrap">
-                        <ReactLinkify
-                          componentDecorator={(href, text, key) =>
-                            href.startsWith(_origin) ? (
-                              <Link href={`${href}?from=home`} key={key}>
-                                <a className="text-blue-500">{text}</a>
-                              </Link>
-                            ) : (
-                              <a
-                                target="_blank"
-                                rel="noreferrer"
-                                href={href}
-                                key={key}
-                                className="text-blue-500"
-                              >
-                                {text}
-                              </a>
-                            )
-                          }
-                        >
-                          {p.text}
-                        </ReactLinkify>
-                      </div>
-                    </div>
-                    <div className="flex justify-between mb-2 mt-2">
-                      <button
-                        title="Добавить в архив"
-                        onClick={() => addToArchive(p.id)}
-                        className="p-2 -m-2 sm:hover:bg-neutral-700 rounded-full"
-                      >
-                        <ArchiveBoxArrowDownIcon className="w-6" />
-                      </button>
-                      <button
-                        title="Поделиться записью"
-                        onClick={() => sharePost(p.id)}
-                        className="p-2 -m-2 sm:hover:bg-neutral-700 rounded-full"
-                      >
-                        <LinkIcon className="w-6" />
-                      </button>
-                    </div>
-                  </div>
+                  <Post
+                    session={session}
+                    from={"home"}
+                    groupId={p.groups.id}
+                    groupData={[p.groups]}
+                    postData={p}
+                    mutateArchive={mutateArchive}
+                    archive={archive}
+                  />
                 ))
               )}
             </InfiniteScroll>
           )
         ) : (
           <div className="space-y-2 mt-2">
-            <div className="bg-neutral-900 h-36 rounded-2xl"></div>
-            <div className="bg-neutral-900 h-36 rounded-2xl"></div>
-            <div className="bg-neutral-900 h-36 rounded-2xl"></div>
-            <div className="bg-neutral-900 h-36 rounded-2xl"></div>
+            <div className="bg-neutral-900 h-[265px] rounded-2xl"></div>
+            <div className="bg-neutral-900 h-[265px] rounded-2xl"></div>
+            <div className="bg-neutral-900 h-[265px] rounded-2xl"></div>
+            <div className="bg-neutral-900 h-[265px] rounded-2xl"></div>
           </div>
         )}
       </Content>
