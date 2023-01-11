bb41137137df35a538967d610a7282aa59228022
bannykhmark1 <bann.mark@yandex.ru>
2023-01-11 14:17:44 +0500

    изменил условия у filter в выоде папок

--- components/Post.js
+++ components/Post.js
@@ -266,7 +266,7 @@ export default function Post({
               <div>
                 <div>
                   {folders
-                    .filter((p) => p.post_id !== postData.id)
+                    .filter((p) => p.post_id === postData.id)
                     .map((p, i) => (
                       <div key={i} className="flex mb-4">
                         <div className="bg-neutral-700 rounded-full p-2 mr-4">
