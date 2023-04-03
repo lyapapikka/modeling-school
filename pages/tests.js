3265e2e01e99b52daf0470d20f5005e262283785
bannykhmark1 <bann.mark@yandex.ru>
2023-04-03 18:21:37 +0500

    сделал canvas

--- pages/tests.js
+++ pages/tests.js
@@ -1,1 +1,31 @@
-export default function Tests() {}
+import React, { useRef, useEffect } from "react";
+
+export default function Tests(props) {
+  const canvasRef = useRef(null);
+
+  const draw = (ctx, frameCount) => {
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#fff";
+    ctx.beginPath();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let frameCount = 0;
+    let animationFrameId;
+
+    const render = () => {
+      frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [draw]);
+
+  return <canvas ref={canvasRef} {...props} width="300px" height="300px" />;
+}
